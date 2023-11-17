7349fa7df0fbcd70501e02426a6588cc1ff1a20b
XenosHu <121327330+XenosHu@users.noreply.github.com>
2023-11-16 22:04:35 -0500

    Update index.py

--- index.py
+++ index.py
@@ -2,22 +2,6 @@ import streamlit as st
 import insert
 import search
 
-# PAGES = {
-#     "Insert Data": insert,
-#     "Search Data": search
-# }
-
-# def main():
-#     st.sidebar.title("Navigation")
-#     selection = st.sidebar.radio("Go to", list(PAGES.keys()))
-
-#     page = PAGES[selection]
-#     page.app()
-
-# if __name__ == "__main__":
-#     main()
-
-
 def main():
     st.sidebar.title("Real Estate Database Management Navigation")
     choice = st.sidebar.selectbox("Choose a page", ["Insert Data", "Search Data"])
