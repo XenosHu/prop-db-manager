95695c83595b8022274d5e4e2b98a0710e279b2e
XenosHu <121327330+XenosHu@users.noreply.github.com>
2023-11-16 21:55:48 -0500

    Update index.py

--- index.py
+++ index.py
@@ -2,17 +2,30 @@ import streamlit as st
 import insert
 import search
 
-PAGES = {
-    "Insert Data": insert,
-    "Search Data": search
-}
+# PAGES = {
+#     "Insert Data": insert,
+#     "Search Data": search
+# }
+
+# def main():
+#     st.sidebar.title("Navigation")
+#     selection = st.sidebar.radio("Go to", list(PAGES.keys()))
+
+#     page = PAGES[selection]
+#     page.app()
+
+# if __name__ == "__main__":
+#     main()
+
 
 def main():
     st.sidebar.title("Navigation")
-    selection = st.sidebar.radio("Go to", list(PAGES.keys()))
+    choice = st.sidebar.selectbox("Choose a page", ["Insert Data", "Search Data"])
 
-    page = PAGES[selection]
-    page.app()
+    if choice == "Insert Data":
+        insert.app()
+    elif choice == "Search Data":
+        search.app()
 
 if __name__ == "__main__":
     main()
