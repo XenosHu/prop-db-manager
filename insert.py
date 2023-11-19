102d681722b7dae4e09347009cc3fbab706ea4fb
jh4578 <jh4578@columbia.edu>
2023-11-19 14:23:48 -0500

    Update insert.py

--- insert.py
+++ insert.py
@@ -10,6 +10,19 @@ def app():
     def get_db_connection():
         connection = mysql.connector.connect(**DATABASE_CONFIG)
         return connection
+
+    def get_builidng_name():
+            connection = get_db_connection()
+            cursor = connection.cursor()
+            building_name_options = ['other']
+            # Check if the building exists
+            cursor.execute("SELECT Building_name FROM Building")
+            building_names = cursor.fetchall()
+            for building_name in building_names:
+                building_name_options.append(building_name[0])
+
+            connection.close()
+            return building_name_options
     
     # Function to add a unit
     def add_unit():
@@ -21,7 +34,7 @@ def app():
             # Input fields
             unit_number = st.text_input("单元号")
             rent_price = st.number_input("租金", min_value=0)
-            unit_type = st.text_input("户型")
+            unit_type = st.selectbox("户型", ['Studio', '1b1b', '2b2b', '2b1b', '3b2b', '3b3b', '4b3b', 'other'])
             floor_plan_image = st.text_input("户型图url")
             unit_image = st.text_input("单元图片url")
             unit_video = st.text_input("单元视频url")
@@ -30,8 +43,10 @@ def app():
             available_date = st.date_input("起租日期")
             washer_dryer = st.checkbox("室内洗烘")
             interest_pp_num = st.number_input("感兴趣人数", min_value=0)
-            building_name = st.text_input("大楼名称")
             
+            building_name_options = get_builidng_name()
+            building_name = st.selectbox("大楼名称", building_name_options)
+
             unit_form_submitted = st.form_submit_button("添加单元")
             
         if unit_form_submitted:
@@ -58,6 +73,7 @@ def app():
             # Check if the building exists
             cursor.execute("SELECT Building_ID FROM Building WHERE Building_name = %s", (building_name,))
             building = cursor.fetchone()
+            st.write(building)
 
             if not building:
                 st.warning("Building not found in the database. Please enter the Building information.")
@@ -90,10 +106,10 @@ def app():
             # Additional Building info fields
             building_name = st.text_input("大楼名称")
             website = st.text_input("网站")
-            location = st.text_input("区域")
+            location = st.selectbox("区域",["New Jersey", "Manhattan upper", "Manhattan mid", "Manhattan lower", "LIC", "Brooklyn",'other'])
             address = st.text_input("详细地址")
             building_description = st.text_area("大楼介绍")
-            building_image = st.text_input("d大楼图片url")
+            building_image = st.text_input("大楼图片url")
             building_location_image = st.text_input("大楼位置图片url")
             postcode = st.text_input("邮编")
             pet = st.checkbox("宠物友好")
