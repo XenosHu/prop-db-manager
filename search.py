4f64b883653485b66c4a0e6543a413873c7e3dba
jh4578 <jh4578@columbia.edu>
2023-11-19 11:08:17 -0500

    更新搜索房源

--- search.py
+++ search.py
@@ -1,28 +1,27 @@
 import streamlit as st
 import mysql.connector
 import pandas as pd
+from st_aggrid import AgGrid, GridOptionsBuilder
 from config import DATABASE_CONFIG
 
 def app():
-    st.title("Property Management Dashboard")
-
-    # Database configuration
-    config = {
-        'user': 'propbotics',
-        'password': 'Propbotics123',
-        'host': 'chatbot.c0xmynwsxhmo.us-east-1.rds.amazonaws.com',
-        'database': 'chatbot',
-        'port': 3306
-    }
+    st.title("搜索房源")
 
     # Function to get database connection
     def get_db_connection():
-        connection = mysql.connector.connect(**config)
+        connection = mysql.connector.connect(**DATABASE_CONFIG)
         return connection
 
     # Function to execute read query
-    def execute_read_query(query):
+    def execute_read_query(query=None):
         connection = get_db_connection()
+        if query is None:
+            # Adjust this default query as per your requirements
+            query = """
+            SELECT Unit.*, Building.Building_name, Building.location
+            FROM Unit
+            JOIN Building ON Unit.building_id = Building.Building_ID
+            """
         df = pd.read_sql(query, connection)
         connection.close()
         return df
@@ -34,78 +33,200 @@ def app():
         cursor.execute(query)
         connection.commit()
         connection.close()
+        
+    with st.form("search_form"):
+        building_name = st.text_input("大楼名称")
+        price = st.number_input("价格", min_value=0, step=1, format='%d')
+        location_options = ["Any", "New Jersey", "Manhattan upper", "Manhattan mid", "Manhattan lower", "LIC", "Brooklyn"]
+        location = st.multiselect("位置", options=location_options, default=["Any"])
+        washer_dryer = st.selectbox("室内洗烘", ["Any", "Yes", "No"])
+        pet = st.selectbox("宠物", ["Any", "Yes", "No"])
+        
+        roomtype_options = ["Any", 'Studio', '1b1b', '2b2b', '2b1b', '3b2b', '4b3b', '3b3b']
+        roomtype = st.multiselect("户型", options=roomtype_options, default=["Any"])
+        roomtype_subunit = st.multiselect("房型", options=["Any", 'bedroom1', 'bedroom2', 'bedroom3', 'living room'], default=["Any"])
 
-    # Display sub_unit and its corresponding unit, building, and user information
-    query = """
-SELECT 
-    sub_unit.sub_unit_id AS sub_unit_id,
-    sub_unit.unit_ID AS sub_unit_unit_ID,
-    sub_unit.room_type AS sub_unit_room_type,
-    sub_unit.sub_rent_price AS sub_unit_sub_rent_price,
-    sub_unit.use_livingroom AS sub_unit_use_livingroom,
-    sub_unit.interest_pp_id AS sub_unit_interest_pp_id,
-    Unit.Unit_ID AS Unit_Unit_ID,
-    Unit.building_id AS Unit_building_id,
-    Unit.unit_number AS Unit_unit_number,
-    Unit.rent_price AS Unit_rent_price,
-    Unit.unit_type AS Unit_unit_type,
-    Unit.floor_plan_image AS Unit_floor_plan_image,
-    Unit.unit_image AS Unit_unit_image,
-    Unit.unit_video AS Unit_unit_video,
-    Unit.description AS Unit_description,
-    Unit.broker_fee AS Unit_broker_fee,
-    Unit.available_date AS Unit_available_date,
-    Unit.washer_dryer AS Unit_washer_dryer,
-    Unit.interest_pp_num AS Unit_interest_pp_num,
-    Building.Building_ID AS Building_Building_ID,
-    Building.Building_name AS Building_Building_name,
-    Building.website AS Building_website,
-    Building.location AS Building_location,
-    Building.address AS Building_address,
-    Building.description AS Building_description,
-    Building.building_image AS Building_building_image,
-    Building.building_location_image AS Building_building_location_image,
-    Building.postcode AS Building_postcode,
-    Building.pet AS Building_pet,
-    Building.application_material AS Building_application_material,
-    Building.amenity_image AS Building_amenity_image,
-    Building.washer_dryer_image AS Building_washer_dryer_image,
-    user.user_id AS user_user_id,
-    user.preference AS user_preference,
-    user.roommate_preference AS user_roommate_preference,
-    user.sex AS user_sex
-FROM 
-    sub_unit
-JOIN 
-    Unit ON sub_unit.unit_ID = Unit.Unit_ID
-JOIN 
-    Building ON Unit.building_id = Building.Building_ID
-JOIN 
-    user ON sub_unit.interest_pp_id = user.user_id;
+        available_start_date = st.date_input("入住时间")
+        available_end_date = st.date_input("至")
+        search_button = st.form_submit_button("搜索")
 
-    """
-    df = execute_read_query(query)
-    st.write("Sub-unit Information", df)
+    # Handle Search
+    if search_button:
 
-    # Update and Delete Section
-    st.subheader("Update or Delete Data")
-    record_to_update = st.text_input("Enter Record ID for Update/Delete")
-    update_field = st.selectbox("Select Field to Update", ["sub_rent_price", "use_livingroom"])
-    new_value = st.text_input("Enter New Value for Update")
-    update_button = st.button("Update Record")
-    delete_button = st.button("Delete Record")
+        include_building = building_name != ""
+        include_unit = price != 0 or washer_dryer != "Any" or pet != "Any" or location != ["Any"]
+        include_subunit = roomtype_subunit != ["Any"]
+        
+        search_query = "SELECT "
+        search_conditions = []
+        join_conditions = ""
 
-    # Handle Update
-    if update_button:
-        update_query = f"UPDATE sub_unit SET {update_field} = '{new_value}' WHERE sub_unit_id = {record_to_update}"
-        execute_write_query(update_query)
-        st.success("Record Updated")
+        if include_subunit:
+            # Query to include Sub_Unit, Unit, and Building
+            search_query += "Sub_Unit.*, Unit.*, Building.* FROM Sub_Unit "
+            join_conditions += "JOIN Unit ON Sub_Unit.unit_ID = Unit.Unit_ID JOIN Building ON Unit.building_id = Building.Building_ID "
+            roomtype_conditions = ["Sub_Unit.room_type = '{}'".format(rt) for rt in roomtype_subunit]
+            search_conditions.append("({})".format(" OR ".join(roomtype_conditions)))
+            if available_start_date:
+                search_conditions.append(f"Unit.available_date >= '{available_start_date}'")
+            if available_end_date:
+                search_conditions.append(f"Unit.available_date <= '{available_end_date}'")
+            st.session_state['include_subunit'] = True
+            st.write("房型:")
+            
+        elif include_unit:
+            # Query to include Unit and Building
+            search_query += "Unit.*, Building.* FROM Unit "
+            join_conditions += "JOIN Building ON Unit.building_id = Building.Building_ID "
+            if available_start_date:
+                search_conditions.append(f"Unit.available_date >= '{available_start_date}'")
+            if available_end_date:
+                search_conditions.append(f"Unit.available_date <= '{available_end_date}'")
+            st.write("单元:")
+            st.session_state['include_unit'] = True
+        else:
+            # Query to include only Building
+            search_query += "* FROM Building "
+            st.write("公寓:")
+            st.session_state['include_building_only'] = True
+            
+    
+        if building_name:
+            search_conditions.append(f"Building.Building_name LIKE '%{building_name}%'")
+        if price:  # Assuming this refers to rent_price in Unit table
+            search_conditions.append(f"Unit.rent_price <= {price}")
+        if "Any" not in location:
+            location_conditions = ["Building.location LIKE '%{}%'".format(loc) for loc in location]
+            search_conditions.append("({})".format(" OR ".join(location_conditions)))
+        if washer_dryer != "Any":
+            washer_dryer_val = 1 if washer_dryer == "Yes" else 0
+            search_conditions.append(f"Unit.washer_dryer = {washer_dryer_val}")
+        if pet != "Any":
+            pet_val = 1 if pet == "Yes" else 0
+            search_conditions.append(f"Building.pet = {pet_val}")
+            
+        if "Any" not in roomtype:
+            roomtype_conditions = ["Unit.unit_type LIKE '%{}%'".format(loc) for loc in roomtype]
+            search_conditions.append("({})".format(" OR ".join(roomtype_conditions)))
 
-    # Handle Delete
-    if delete_button:
-        delete_query = f"DELETE FROM sub_unit WHERE sub_unit_id = {record_to_update}"
-        execute_write_query(delete_query)
-        st.success("Record Deleted")
+
+        final_query = search_query + join_conditions
+        if search_conditions:
+            final_query += "WHERE " + " AND ".join(search_conditions)
+
+        df = execute_read_query(final_query)
+        st.session_state['search_results'] = df
+
+    # Display Search Results
+    if 'search_results' in st.session_state:
+        df = st.session_state['search_results']
+
+        # Set up AgGrid options for editable grid
+        gb = GridOptionsBuilder.from_dataframe(df)
+        gb.configure_default_column(editable=True, minWidth=150)
+        gb.configure_selection('multiple', use_checkbox=True)
+        grid_options = gb.build()
+
+        # Display the grid
+        grid_response = AgGrid(
+            df, 
+            gridOptions=grid_options,
+            height=300, 
+            width='100%',
+            data_return_mode='AS_INPUT', 
+            update_mode='MODEL_CHANGED',
+            fit_columns_on_grid_load=True
+        )
+
+        if 'data' in grid_response:
+            updated_df = grid_response['data']
+            if not updated_df.equals(df):
+                st.session_state['updated_df'] = updated_df
+
+        # Store selected rows for deletion
+        selected = grid_response['selected_rows']
+        if selected:
+            st.session_state['selected_for_deletion'] = selected
+            #st.write("Selected rows:", selected)
+
+        # Confirm Update Button
+        if st.button('Confirm Update'):
+
+            is_building_only = st.session_state.get('include_building_only', False)
+            is_unit_included = st.session_state.get('include_unit', False)
+            is_subunit_included = st.session_state.get('include_subunit', False)
+
+            building_columns = [
+                "Building_name", "website", "location", "address",
+                "building_description", "building_image", "building_location_image",
+                "postcode", "pet", "application_material", "amenity_image", "washer_dryer_image"
+            ]
+
+            unit_columns = [
+                "unit_number", "rent_price", "unit_type",
+                "floor_plan_image", "unit_image", "unit_video", "unit_description",
+                "broker_fee", "available_date", "washer_dryer", "interest_pp_num"
+            ]
+
+            user_columns = [
+                "user_id", "preference", "roommate_preference", "sex"
+            ]
+
+            sub_unit_columns = [
+                 "room_type", "sub_rent_price",
+                "use_livingroom", "interest_pp_id"
+            ]
+            
+            if 'updated_df' in st.session_state:
+                updated_df = st.session_state['updated_df']
+
+                # Handle updates for Building, Unit, and Sub_Unit
+                for i in updated_df.index:
+                    if is_subunit_included:
+                        # Construct and execute UPDATE query for Sub_Unit
+                        sub_unit_update_query = "UPDATE Sub_Unit SET "
+                        sub_unit_update_query += ", ".join([f"{col} = '{updated_df.at[i, col]}'" for col in updated_df.columns if col in sub_unit_columns])
+                        sub_unit_update_query += f" WHERE sub_unit_id = {updated_df.at[i, 'sub_unit_id']}"
+                        execute_write_query(sub_unit_update_query)
+
+                    if is_unit_included and not is_subunit_included:
+                        # Construct and execute UPDATE query for Unit
+                        unit_update_query = "UPDATE Unit SET "
+                        unit_update_query += ", ".join([f"{col} = '{updated_df.at[i, col]}'" for col in updated_df.columns if col in unit_columns])
+                        unit_update_query += f" WHERE Unit_ID = {updated_df.at[i, 'Unit_ID']}"
+                        execute_write_query(unit_update_query)
+
+                    if is_building_only:
+                        # Construct and execute UPDATE query for Building
+                        building_update_query = "UPDATE Building SET "
+                        building_update_query += ", ".join([f"{col} = '{updated_df.at[i, col]}'" for col in updated_df.columns if col in building_columns])
+                        building_update_query += f" WHERE Building_ID = {updated_df.at[i, 'Building_ID']}"
+                        execute_write_query(building_update_query)
+
+                # Handle deletions
+                if 'selected_for_deletion' in st.session_state:
+                    for row in st.session_state['selected_for_deletion']:
+                        if is_subunit_included:
+                            # DELETE FROM Sub_Unit WHERE sub_unit_id = value
+                            sub_unit_delete_query = f"DELETE FROM Sub_Unit WHERE sub_unit_id = {row['sub_unit_id']}"
+                            execute_write_query(sub_unit_delete_query)
+
+                        elif is_unit_included:
+                            # DELETE FROM Unit WHERE unit_id = value
+                            unit_delete_query = f"DELETE FROM Unit WHERE Unit_ID = {row['Unit_ID']}"
+                            execute_write_query(unit_delete_query)
+
+                        elif is_building_only:
+                            # DELETE FROM Building WHERE building_id = value
+                            building_delete_query = f"DELETE FROM Building WHERE Building_ID = {row['Building_ID']}"
+                            execute_write_query(building_delete_query)
+                            
+                del st.session_state['updated_df']  # Clear the updated data from the session state
+                st.session_state['include_building_only'] = False
+                st.session_state['include_unit'] = False
+                st.session_state['include_subunit'] = False
+                st.success("Database Updated Successfully")
+
 
 if __name__ == "__main__":
     app()
