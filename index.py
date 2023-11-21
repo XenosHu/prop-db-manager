4f32e75075b96be3e68b69a51e96a2edb333d725
XenosHu <121327330+XenosHu@users.noreply.github.com>
2023-11-21 15:34:03 -0500

    Update index.py

--- index.py
+++ index.py
@@ -57,6 +57,7 @@ def logout():
 
 if 'password_correct' in st.session_state and st.session_state['password_correct']:
     def main():
+        st.set_page_config(layout="wide", initial_sidebar_state="auto")
         st.title("房源数据管理")
         st.sidebar.title("目录")
         choice = st.sidebar.selectbox("选择", ["添加公寓", "添加单元", "搜索房源", "测试"])
