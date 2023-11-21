8017348e0afa7bac86f32844eb0a525af6e21338
XenosHu <121327330+XenosHu@users.noreply.github.com>
2023-11-21 14:27:56 -0500

    Update insert_unit.py

--- insert_unit.py
+++ insert_unit.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import mysql.connector
 from datetime import datetime
-# from config import get_database_config
-import config
+from config import get_database_config
+
 
 def app():
     st.title("添加单元")
