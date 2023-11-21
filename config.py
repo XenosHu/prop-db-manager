a5d7cef7f993e3570e9bbee0b1e6cc7454a10b40
XenosHu <121327330+XenosHu@users.noreply.github.com>
2023-11-21 15:27:05 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,22 +1,5 @@
 # config.py
 
-# import streamlit as st
-# db_user = st.secrets["db_config"]["user"]
-# db_password = st.secrets["db_config"]["password"]
-# db_host = st.secrets["db_config"]["host"]
-# db_database = st.secrets["db_config"]["database"]
-# db_port = st.secrets["db_config"]["port"]
-
-# # Database configuration
-# DATABASE_CONFIG = {
-#     'user': db_user,
-#     'password': db_password,
-#     'host': db_host,
-#     'database': db_database,
-#     'port': db_port
-# }
-
-
 import streamlit as st
 
 # def get_database_config():
