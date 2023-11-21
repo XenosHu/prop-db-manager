578132e7baeb58f3a2f5ddfe94adf34f2677e7b4
XenosHu <121327330+XenosHu@users.noreply.github.com>
2023-11-21 14:11:47 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,17 @@
 # config.py
 
+
+db_user = st.secrets["db_config"]["user"]
+db_password = st.secrets["db_config"]["password"]
+db_host = st.secrets["db_config"]["host"]
+db_database = st.secrets["db_config"]["database"]
+db_port = st.secrets["db_config"]["port"]
+
 # Database configuration
 DATABASE_CONFIG = {
-    'user': 'propbotics',
-    'password': 'Propbotics123',
-    'host': 'chatbot.c0xmynwsxhmo.us-east-1.rds.amazonaws.com',
-    'database': 'chatbot',
-    'port': 3306
+    'user': db_user,
+    'password': db_password,
+    'host': db_host,
+    'database': db_database,
+    'port': db_port
 }
