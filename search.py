4ec50bb372b8d366feec6447f48ab85b3b425bdc
XenosHu <121327330+XenosHu@users.noreply.github.com>
2023-11-21 14:50:46 -0500

    Update search.py

--- search.py
+++ search.py
@@ -2,8 +2,7 @@ import streamlit as st
 import mysql.connector
 import pandas as pd
 from st_aggrid import AgGrid, GridOptionsBuilder
-from config import get_database_config
-
+from config import DATABASE_CONFIG
 
 def app():
     st.title("搜索房源")
