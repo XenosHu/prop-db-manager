ea4dfa22d4395c7dc71c2b0e3dfda13d769a881a
XenosHu <121327330+XenosHu@users.noreply.github.com>
2023-12-04 13:51:50 -0500

    Update insert_unit.py

--- insert_unit.py
+++ insert_unit.py
@@ -97,19 +97,19 @@ def app():
                 unit_number = st.text_input("单元号")
                 rent_price = st.number_input("租金", min_value=0)
                 floorplan = st.selectbox("户型", ['Studio', '1b1b', '2b2b', '2b1b', '3b2b', '3b3b', '4b3b', 'other'])
-                floorplan_image = st.text_input("户型图像URL")
                 available_date = st.date_input("起租日期")
-                description = st.text_area("单元描述")
-                broker_fee = st.number_input("中介费", min_value=0)
                 sqft = st.number_input("单元面积", min_value=0)
+                washer_dryer = st.selectbox("室内洗烘", ["Any","Yes", "No"])
+                description = st.text_area("单元描述")
     
             with col2:
                 # Column 2 fields
                 unit_image = st.text_input("单元图片URL")
                 unit_video = st.text_input("单元视频URL")
-                concession = st.text_input("优惠")
+                floorplan_image = st.text_input("户型图像URL")
                 direction = st.selectbox("房间朝向", ["N", "S", "E", "W", "NE", "NW", "SE", "SW"])
-                washer_dryer = st.selectbox("室内洗烘", ["Any","Yes", "No"])
+                concession = st.text_input("优惠")
+                broker_fee = st.number_input("中介费", min_value=0)
                 interest_pp_num = st.number_input("感兴趣人数", min_value=0)
     
             unit_form_submitted = st.form_submit_button("添加单元")
@@ -136,54 +136,6 @@ def app():
             
             # Assuming you have a function to handle the database connection
             connection = get_db_connection()   
-    # # Function to add a unit
-    # def add_unit():
-    #     if 'unit_data' not in st.session_state:
-    #         st.session_state['unit_data'] = None
-    #     with st.form("add_unit_form"):
-    #         col1, col2 = st.columns(2)
-
-    #         with col1:
-    #             # Column 1 fields
-    #             building_name_options = get_building_name()
-    #             building_name = st.selectbox("公寓名称", building_name_options)
-    #             unit_number = st.text_input("单元号")
-    #             rent_price = st.number_input("租金", min_value=0)
-    #             floorplan = st.selectbox("户型", ['Studio', '1b1b', '2b2b', '2b1b', '3b2b', '3b3b', '4b3b', 'other'])
-    #             available_date = st.date_input("起租日期")
-    #             description = st.text_area("单元描述")
-                
-
-    #         with col2:
-    #             # Column 2 fields
-    #             floor_plan_image = st.text_input("户型图url")
-    #             unit_image = st.text_input("单元图片url")
-    #             unit_video = st.text_input("单元视频url")
-    #             broker_fee = st.number_input("中介费", min_value=0)
-    #             interest_pp_num = st.number_input("感兴趣人数", min_value=0)
-    #             washer_dryer = st.selectbox("室内洗烘",["Any","Yes", "No"])
-
-    #         unit_form_submitted = st.form_submit_button("添加单元")
-            
-    #     if unit_form_submitted:         
-    #             # 保存Unit数据到会话状态
-    #         st.session_state['unit_data'] = {
-    #                 'unit_number': unit_number,
-    #                 'rent_price': rent_price,
-    #                 'floorplan': floorplan,
-    #                 'floor_plan_image': floor_plan_image,
-    #                 'unit_image': unit_image,
-    #                 'unit_video': unit_video,
-    #                 'unit_description': description,
-    #                 'broker_fee': broker_fee,
-    #                 'available_date': available_date,
-    #                 'washer_dryer': washer_dryer,
-    #                 'interest_pp_num': interest_pp_num,
-    #                 'building_name': building_name
-    #             }
-                
-    #         connection = get_db_connection()
-            
             cursor = connection.cursor()
 
             # Check if the building exists
