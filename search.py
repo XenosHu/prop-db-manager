845534a9a2bd5a1112d5926122bb9bdc2da94f0e
jh4578 <116768893+jh4578@users.noreply.github.com>
2023-12-15 10:47:04 -0500

    Update search.py

--- search.py
+++ search.py
@@ -14,7 +14,7 @@ def app():
 
     # Function to execute read query
     def execute_read_query(query=None):
-        st.write(query)
+        # st.write(query)
         connection = get_db_connection()
         if query is None:
             # Adjust this default query as per your requirements
