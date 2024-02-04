2744cdfcc548651ca2fa323591a29b78f862bed4
jh4578 <116768893+jh4578@users.noreply.github.com>
2024-02-04 17:31:23 -0500

    Update search.py

--- search.py
+++ search.py
@@ -65,7 +65,9 @@ def app():
             roomtype_options = ["Any", 'Studio', '1b1b', '2b2b', '2b1b', '3b2b', '4b3b', '3b3b']
             roomtype = st.multiselect("户型", options=roomtype_options, default=["Any"])
             roomtype_subunit = st.multiselect("房型", options=["Any", "All",'bedroom1', 'bedroom2', 'bedroom3', 'living_room'], default=["Any"])
-            movein_date = st.date_input("入住时间")
+            if_time = st.checkbox('搜索时间',value = False)
+            if if_time:
+                movein_date = st.date_input("入住时间")
             pet = st.checkbox("宠物友好", value=False)
             washer_dryer = st.checkbox("室内洗烘", value=False)
             on_market = st.checkbox("On Market", value=False)
@@ -127,8 +129,8 @@ def app():
                 roomtype_subunit = ['bedroom1', 'bedroom2', 'bedroom3', 'living_room']
             roomtype_conditions = ["sub_unit.room_type = '{}'".format(rt) for rt in roomtype_subunit]
             search_conditions.append("({})".format(" OR ".join(roomtype_conditions)))
-            
-            search_conditions.append(f"Unit.available_date <= '{movein_date}' AND Unit.movein_before >= '{movein_date}'")
+            if movein_date:
+                search_conditions.append(f"Unit.available_date <= '{movein_date}' AND Unit.movein_before >= '{movein_date}'")
         
             if on_market:
                 search_conditions.append("Unit.on_market = 1")
@@ -166,8 +168,8 @@ def app():
                                 Unit.unit_id,
                                 Building.building_id FROM Unit """
             join_conditions += "JOIN Building ON Unit.building_id = Building.building_id "
-            
-            search_conditions.append(f"Unit.available_date <= '{movein_date}' AND Unit.movein_before >= '{movein_date}'")
+            if movein_date:
+                search_conditions.append(f"Unit.available_date <= '{movein_date}' AND Unit.movein_before >= '{movein_date}'")
 
             if on_market:
                 search_conditions.append("Unit.on_market = 1")
