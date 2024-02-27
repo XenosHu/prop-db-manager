64e483bf4e95a6d4877855707f84b2e15c1d5d56
jh4578 <116768893+jh4578@users.noreply.github.com>
2024-02-27 14:31:16 -0500

    Update update_user.py

--- update_user.py
+++ update_user.py
@@ -136,7 +136,7 @@ def app():
         # 插入新用户数据到数据库
         insert_query = f"""
         INSERT INTO user (wechat_id, preference, chatbot_wx_id, sche_listing,is_group)
-        VALUES ('{new_wechat_id}', '{new_preference}', '{new_chatbot_wx_id}', {new_sche_listing_value},{is_group})
+        VALUES ('{new_wechat_id}', '{new_preference}', '{new_chatbot_wx_id}', {new_sche_listing},{is_group})
         """
         execute_write_query(insert_query)
         st.success("用户添加成功！")
