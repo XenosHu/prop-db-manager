5e2a00ee158c264e32589e9e2104380b7d384cbb
jh4578 <116768893+jh4578@users.noreply.github.com>
2024-02-27 14:25:05 -0500

    Update update_user.py

--- update_user.py
+++ update_user.py
@@ -51,7 +51,7 @@ def app():
     # Handle Search
     if search_user:
         search_query = """
-        SELECT user_id, preference, roommate_preference, sex, wechat_id, conversation, chatbot_wx_id, sche_listing
+        SELECT user_id, preference, roommate_preference, sex, wechat_id, conversation, chatbot_wx_id, sche_listing, is_group
         FROM user
         WHERE 1=1
         """
@@ -96,7 +96,8 @@ def app():
                         'sex': 'sex',
                         'wechat_id': 'wechat_id',
                         'chatbot_wx_id': 'chatbot_wx_id',
-                        'sche_listing': 'sche_listing'
+                        'sche_listing': 'sche_listing',
+                        'is_group':'is_group'
                     }
 
                     for i in updated_df.index:
