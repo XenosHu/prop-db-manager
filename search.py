3386bc1611c181238e73ec19c39205833ceed368
jh4578 <116768893+jh4578@users.noreply.github.com>
2024-04-16 17:54:57 -0400

    Update search.py

--- search.py
+++ search.py
@@ -320,7 +320,7 @@ def app():
         # Store selected rows for deletion
         selected = grid_response['selected_rows']
         st.write(selected)
-        if selected:
+        if len(selected) > 0:
             st.write('after_select')
             st.session_state['selected_for_deletion'] = selected
             #st.write("Selected rows:", selected)
