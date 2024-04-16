0ff478dbf91310a7c2a6578d08e28139f0f02c76
jh4578 <116768893+jh4578@users.noreply.github.com>
2024-04-16 18:05:28 -0400

    Update search.py

--- search.py
+++ search.py
@@ -342,7 +342,7 @@ def app():
 
                     else:
                         # DELETE FROM Building WHERE building_id = value
-                        st.write(row)
+                        st.write(st.session_state['selected_for_deletion'])
                         building_delete_query = f"DELETE FROM Building WHERE building_id = {int(row['building_id'])}"
                         execute_write_query(building_delete_query)
                 
